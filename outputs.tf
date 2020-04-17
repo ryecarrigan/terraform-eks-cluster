5684b7f8e68017296acaa49586b0df36c83187cb
Rye Carrigan <rcarrigan@cloudbees.com>
2020-04-17 19:26:43 -0400

    Adding additional outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "bastion_security_group_id" {
   value = aws_security_group.bastion.id
 }
+
+output "control_plane_security_group_id" {
+  value = aws_security_group.control_plane.id
+}
+
+output "node_security_group_id" {
+  value = aws_security_group.node.id
+}
