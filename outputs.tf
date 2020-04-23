b3be15ce26320ac6f8269bf04a789c9c01c330cb
Rye Carrigan <rcarrigan@cloudbees.com>
2020-04-23 12:15:50 -0400

    Adding cluster_name to output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "bastion_security_group_id" {
   value = aws_security_group.bastion.id
 }
 
+output "cluster_name" {
+  value = aws_eks_cluster.cluster.name
+}
+
 output "control_plane_security_group_id" {
   value = aws_security_group.control_plane.id
 }
